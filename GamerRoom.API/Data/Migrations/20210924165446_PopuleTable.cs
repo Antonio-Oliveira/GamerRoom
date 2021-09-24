9a156a692955d0976d83f58041b34952c9e6d740
Antonio Oliveira <antonio_jorge15@hotmail.com>
2021-09-24 14:47:49 -0300

    Inclusão do await em auth-register

--- GamerRoom.API/Data/Migrations/20210924165446_PopuleTable.cs
+++ GamerRoom.API/Data/Migrations/20210924165446_PopuleTable.cs
@@ -6,7 +6,7 @@ namespace GamerRoom.API.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO TB_GAMES(ID, Name, Description, Developer, Publisher, Platform, Genre, Mode, ReleaseDate) VALUES('aaf33b90-1e24-47ae-a4a6-c22d30fbce54','Free Fire', 'Free Fire[a] é um jogo eletrônico mobile de ação-aventura do gênero battle royale, criado pela desenvolvedora vietnamita 111dots Studio e publicado pela Garena.[2] O jogo obteve um beta aberto em novembro de 2017 e foi lançado oficialmente para Android de iOS em 4 de dezembro de 2017.','111dots Studio','Garena','Android;iOS','battle royale','Multijogador','0001-01-01 00:00:00.0000000')");
+            migrationBuilder.Sql("INSERT INTO TB_GAMES(ID, Name, Description, Developer, Publisher, Platform, Genre, Mode, ReleaseDate) VALUES('aaf33b90-1e24-47ae-a4a6-c22d30fbce54','Free Fire', 'Free Fire é um jogo eletrônico mobile de ação-aventura do gênero battle royale, criado pela desenvolvedora vietnamita 111dots Studio e publicado pela Garena.[2] O jogo obteve um beta aberto em novembro de 2017 e foi lançado oficialmente para Android de iOS em 4 de dezembro de 2017.','111dots Studio','Garena','Android;iOS','battle royale','Multijogador','0001-01-01 00:00:00.0000000')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
